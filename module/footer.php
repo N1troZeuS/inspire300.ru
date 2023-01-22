26f6a5eb8b00e07a74bbfd648f468876717cf513
INSPIRE <77198899+N1troZeuS@users.noreply.github.com>
2023-01-22 16:57:58 +0300

    Add files via upload

---
+++ module/footer.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <footer class="py-5">
+        <div class="row text-green">
+            <div class="col-6 col-md-2 mb-3">
+                <h5>Основное</h5>
+                <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="/index" class="nav-link p-0 text-muted">Главная</a></li>
+                <li class="nav-item mb-2"><a href="/me" class="nav-link p-0 text-muted">Обо мне</a></li>
+                <li class="nav-item mb-2"><a href="/projects" class="nav-link p-0 text-muted">Проекты</a></li>
+                <li class="nav-item mb-2"><a href="/disclaimer" class="nav-link p-0 text-muted">Дисклеймер</a></li>
+                <li class="nav-item mb-2"><a href="/contact" class="nav-link p-0 text-muted">Контакты</a></li>
+                <li class="nav-item mb-2"><a href="/galery" class="nav-link p-0 text-muted">Галерея</a></li>
+                <li class="nav-item mb-2"><a href="https://github.com/" class="nav-link p-0 text-muted">Код сайта</a></li>
+            </div>
+            <div class="col-6 col-md-2 mb-3">
+                <h5>Услуги</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="/order" class="nav-link p-0 text-muted">Заказать сайт</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class=" text-green d-flex flex-column flex-sm-row justify-content-between py-4 my-4">
+            <p><?php echo date("d.m.y") ?>. Все права защищены. inspired by INSPIRE.</p>
+        </div>
+    </footer>
+</div>
---
+++ module/header.php
@@ -0,0 +1,18 @@
+<header>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="icon" href="img/favicon.ico" type="image/png">
+</header>
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <span class="fs-4 text-green">INSPIRE</span>
+        </a>
+        <ul class="nav nav-pills">
+            <li><a href="/me" class="nav-link">Обо мне</a></li>
+            <li><a href="/projects" class="nav-link">Проекты</a></li>
+            <li><a href="/contact" class="nav-link">Контакты</a></li>
+        </ul>
+    </header>
+</div>
+</div>
